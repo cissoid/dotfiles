cee290ed782574b187688e1badacd4a10dfc8367
cissoid <yangtukun1412@gmail.com>
2016-05-06 16:01:16 +0800

    Add, ipython config.

---
+++ ipython/ipython_config.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+# File Name: ipython_config.py
+# Author:
+# Created At: 2016-05-06 15:58:37
+# Last Modified: 2016-05-06 15:59:28
+c = get_config()
+c.InteractiveShellApp.extensions = [
+    'powerline.bindings.ipython.post_0_11'
+]
