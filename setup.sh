570c002352232aaeeae61391b247a325873257c1
cissoid <yangtukun1412@gmail.com>
2016-05-19 10:11:26 +0800

    Updated.

--- setup.sh
+++ setup.sh
@@ -13,16 +13,16 @@ linkto () {
 }
 
 files=(
-    $SRC_DIR/bash/bashrc    $TAR_DIR/.bashrc
-    $SRC_DIR/bash/inputrc   $TAR_DIR/.inputrc
-    $SRC_DIR/flake8/flake8  $TAR_DIR/.config/flake8
-    $SRC_DIR/git/gitconfig  $TAR_DIR/.gitconfig
+    $SRC_DIR/bash/bashrc                $TAR_DIR/.bashrc
+    $SRC_DIR/bash/inputrc               $TAR_DIR/.inputrc
+    $SRC_DIR/flake8/flake8              $TAR_DIR/.config/flake8
+    $SRC_DIR/git/gitconfig              $TAR_DIR/.gitconfig
     $SRC_DIR/ipython/ipython_config.py  $TAR_DIR/.ipython/profile_default/ipython_config.py
-    $SRC_DIR/powerline  $TAR_DIR/.config/powerline
-    $SRC_DIR/tern/tern-config   $TAR_DIR/.tern-config
-    $SRC_DIR/tmux/tmux.conf $TAR_DIR/.tmux.conf
-    $SRC_DIR/vim/vimrc  $TAR_DIR/.vimrc
-    $SRC_DIR/vim    $TAR_DIR/.vim
+    $SRC_DIR/powerline                  $TAR_DIR/.config/powerline
+    $SRC_DIR/tern/tern-config           $TAR_DIR/.tern-config
+    $SRC_DIR/tmux/tmux.conf             $TAR_DIR/.tmux.conf
+    $SRC_DIR/vim/vimrc                  $TAR_DIR/.vimrc
+    $SRC_DIR/vim                        $TAR_DIR/.vim
 )
 
 for (( i=0; i<${#files[@]}; i=i+2 ))
