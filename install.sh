640315488dbf6ebd49d18b911ca06e8dadf43d22
cissoid <yangtukun1412@gmail.com>
2016-11-11 12:04:32 +0800

    Modify, use stow link files.

--- install.sh
+++ install.sh
@@ -1,30 +1,17 @@
-# /usr/bin/env bash
+#!/bin/bash
 
-SRC_DIR=`dirname \`realpath ${0}\``
-TAR_DIR=$HOME
+if [ ! `which stow` ]; then
+    echo Ready to install stow...
 
-linkto () {
-    if [ -d $2 -o -f $2 ]
-    then
-        echo "$2 exist, ignore it..."
-        return 1
+    if [ "$(uname)" == "Darwin" ]; then
+        brew install stow
     fi
-    ln -s $1 $2
-}
+fi
 
-files=(
-    $SRC_DIR/bash/bashrc                $TAR_DIR/.bashrc
-    $SRC_DIR/bash/inputrc               $TAR_DIR/.inputrc
-    $SRC_DIR/git/gitconfig              $TAR_DIR/.gitconfig
-    # $SRC_DIR/powerline                  $TAR_DIR/.config/powerline
-    $SRC_DIR/tmux/tmux.conf             $TAR_DIR/.tmux.conf
-    $SRC_DIR/vim/vimrc                  $TAR_DIR/.vimrc
-    $SRC_DIR/vim                        $TAR_DIR/.vim
-)
+stow bash
+stow git
+stow tmux
+stow vim
 
-for (( i=0; i<${#files[@]}; i=i+2 ))
-do
-    src=${files[i]}
-    dest=${files[i+1]}
-    linkto $src $dest
-done
+stow formatters
+stow linters
