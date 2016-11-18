3bfa09065a7ea88d1dead5f450cc64713f9fcf70
cissoid <yangtukun1412@gmail.com>
2016-11-18 17:20:17 +0800

    Add, vscode config.

--- install.sh
+++ install.sh
@@ -1,22 +1,76 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ ! `which stow` ]; then
-    echo Ready to install stow...
-
+function get_platform() {
     if [ "$(uname)" == "Darwin" ]; then
-        brew install stow
-    elif [ "$(lsb_release -i | awk '{print $3}')" == "Ubuntu" ]; then
-        sudo apt install stow
+        echo "macOS"
     else
-        echo Unknown platform.
-        exit -1
+        echo "$(lsb_release -i | awk '{print $3}')"
     fi
-fi
+}
 
-stow bash
-stow git
-stow tmux
-stow vim
+function install_stow() {
+    if [ ! `which stow` ]; then
+        echo "Ready to install stow..."
 
-stow formatters
-stow linters
+        local platform=$(get_platform)
+
+        if [ "$platform" == "macOS" ]; then
+            brew install stow
+        elif [ "$platform" == "Ubuntu" ]; then
+            sudo apt install stow
+        else
+            echo "Unknown platform."
+            exit 1
+        fi
+    fi
+}
+
+function install() {
+    install_stow
+
+    stow bash
+    stow git
+    stow tmux
+    stow vim
+
+    local platform=$(get_platform)
+    if [ "$platform" == "macOS" ]; then
+        stow -t "$HOME/Library/Application Support/Code/User" vscode
+    fi
+
+    stow formatters
+    stow linters
+}
+
+function install_linter() {
+    local install_maps=(
+        "pip;flake8;flake8"
+    )
+    install_maps+=(
+        "npm;eslint;eslint"
+    )
+
+    for map in ${install_maps[@]}
+    do
+        echo $map
+    done
+}
+
+function usage() {
+    echo "Usage: $0 [linter|formatter]"
+}
+
+
+case "$1" in 
+    "")
+        install $*
+        ;;
+    "linter")
+        install_linter $*
+        ;;
+    "formatter")
+        install_formatter $*
+        ;;
+    *)
+        usage $*
+esac
